4e22ddbeae5519ad6dc77b7ae71557ad4df7e8ee
Fastlane <jean.christophe.henry.pro@gmail.com>
2019-11-26 00:58:21 +0100

    Première version qui fonctionne avec l'API Facebook.

---
+++ src/test/java/com/ynov/postcross/PostcrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.postcross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
